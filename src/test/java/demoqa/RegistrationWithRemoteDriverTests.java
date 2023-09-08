c159fd477910b1aa188bb02b4d712231163a103c
rzzkv <rzzkv@yandex.ru>
2023-09-08 14:05:26 +0500

    added steps in remote

--- src/test/java/demoqa/RegistrationWithRemoteDriverTests.java
+++ src/test/java/demoqa/RegistrationWithRemoteDriverTests.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Tags;
 import org.junit.jupiter.api.Test;
 
+import static io.qameta.allure.Allure.step;
+
 public class RegistrationWithRemoteDriverTests extends TestBaseRemote {
 
     @Owner("rzzkv")
@@ -17,25 +19,35 @@ public class RegistrationWithRemoteDriverTests extends TestBaseRemote {
     @Test
     void fillFormTest() {
 
-        registrationPage
-                .openPage()
-                .setFirstName(data.firstName)
-                .setLastName(data.lastName)
-                .setEmail(data.userEmail)
-                .setGender()
-                .setNumber(data.userNumber)
-                .setDateOfBirth()
-                .uploadPicture(data.picture)
-                .setAddress(data.currentAddress)
-                .setSubject(data.subject)
-                .setHobbies(data.hobbies)
-                .submit();
+        step("Open registration Page", () -> {
+            registrationPage.openPage();
+        });
 
-        registrationPage.verifyResultsModalAppears()
-                .verifyResult("Student Name", data.firstName + " " + data.lastName)
-                .verifyResult("Student Email", data.userEmail)
-                .verifyResult("Mobile", data.userNumber)
-                .verifyResult("Date of Birth", "16 April,2000");
+        step("Fulfill fields with data", () -> {
+            registrationPage
+                    .openPage()
+                    .setFirstName(data.firstName)
+                    .setLastName(data.lastName)
+                    .setEmail(data.userEmail)
+                    .setGender()
+                    .setNumber(data.userNumber)
+                    .setDateOfBirth()
+                    .uploadPicture(data.picture)
+                    .setAddress(data.currentAddress)
+                    .setSubject(data.subject)
+                    .setHobbies(data.hobbies);
+        });
 
+        step("Submit", () -> {
+            registrationPage.submit();
+        });
+
+        step("Verify Results", () -> {
+            registrationPage.verifyResultsModalAppears()
+                    .verifyResult("Student Name", data.firstName + " " + data.lastName)
+                    .verifyResult("Student Email", data.userEmail)
+                    .verifyResult("Mobile", data.userNumber)
+                    .verifyResult("Date of Birth", "16 April,2000");
+        });
     }
 }
